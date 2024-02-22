a56ab651285b6020693fb9f15b84b6e35ed0c8c2
Paulo André <pauloandre7@users.noreply.github.com>
2024-02-22 10:47:01 -0300

    init project

---
+++ src/test/java/com/pauloandre7/jokesapi/JokesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pauloandre7.jokesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
